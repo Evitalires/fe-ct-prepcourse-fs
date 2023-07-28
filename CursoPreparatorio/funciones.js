60eafd7addfd129fb652b94ae890be91bfdefe7a
JairRamirez <evitalires@gmail.com>
2023-07-28 12:46:31 -0500

    JavaScript main concepts

---
+++ CursoPreparatorio/funciones.js
@@ -0,0 +1,20 @@
+//Función: sumaTres
+
+function sumaDos(x) {
+    return x + 2;
+}
+
+var sumaCuatro = function (x) {
+    return x + 4
+}
+
+var sumaTres = (x) => {
+    return console.log(x + 3);;
+  }
+  sumaTres(5)
+  
+  //Función: cuidadoConElConsoleLog
+  function cuidadoConElConsoleLog(nombre) {
+      console.log(nombre);
+      return nombre;
+    }
---
+++ CursoPreparatorio/variables.js
@@ -0,0 +1,20 @@
+//String
+var bootcamp = 'Henry';
+
+//Numeros
+var numeroEntero = 17;
+
+var numeroNegativo = -4;
+
+var numeroDecimal = 11.75;
+
+//Booleanos
+var estoyEntendiendo = true;
+
+var estoyAburrido = false;
+
+
+//undefined
+var cajon1;
+
+console.log(cajon1);
